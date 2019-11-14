9bd2b519e5a6cd6a91cd0ce5e5078dcfceccefd1
mattiaseyram <mattias.eyram@shopify.com>
2019-11-13 22:54:49 -0500

    Added user creation, auth, cloud function call

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+try {
+    const serviceAccount = require('../serviceAccountKey.json');
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+    console.warn('Using ../serviceAccountKey');
+} catch (err) {
+    admin.initializeApp();
+    console.warn('Initializing default firebase admin.');
+}
+
+exports.helloWorld = functions.https.onCall(async (data, context) => {
+    try {
+        const { name = "Anonymous" } = data;
+        return { message: `Hello ${name}!`};
+    } catch (err) {
+        throw new functions.https.HttpsError('unknown', 'Error in helloWorld: ' + err.message);
+    }
+});
---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5TFI1EmwOjKuoCsalVeMjd3mu2qXC33c",
+    authDomain: "codejam-starter.firebaseapp.com",
+    databaseURL: "https://codejam-starter.firebaseio.com",
+    projectId: "codejam-starter",
+    storageBucket: "codejam-starter.appspot.com",
+    messagingSenderId: "168385856355",
+    appId: "1:168385856355:web:1ff0c425d5c9663c500956"
+};
+
+export default firebaseConfig;
